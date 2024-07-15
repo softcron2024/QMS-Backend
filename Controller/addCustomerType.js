a4c9abf14a2114164ddaf7cd5d7cf4384eb2770a
manishmanume <149936975+manishmanume@users.noreply.github.com>
2024-07-15 15:15:54 +0530

    some changes in Add customer type

--- Controller/addCustomerType.js
+++ Controller/addCustomerType.js
@@ -3,18 +3,18 @@ const asyncHandler = require("../utils/asynchandler");
 
 const addCustomerType = asyncHandler(async (req, res) => {
 
-    const { customer_type_name } = req.body
+    const { customer_type_name, customer_type_color } = req.body
 
-    if (!customer_type_name) {
+    if (!customer_type_name || !customer_type_color) {
         return res.status(404).json({ ResponseCode: 0, message: "Customer type name is required" })
     }
 
-    Connection.query("CALL SPinsertcustomertype(?)", [customer_type_name], (error, results) => {
+    Connection.query("CALL SPinsertcustomertype(?,?)", [customer_type_name, customer_type_color], (error, results) => {
         if (error) {
             console.log(error);
             return res.status(500).json({ message: "Error adding new customer type, Try Again!!!" });
         }
-        return res.status(200).json({ message: results[0][0] })
+        return res.status(200).json({ResponseCode: 1, message: results[0][0] })
     });
 })
 
