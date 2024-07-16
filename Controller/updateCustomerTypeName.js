a0060adc70d1139e77eac1622232db001ef4b9ef
softcron2024 <softcron2024@gmail.com>
2024-07-16 16:16:43 +0530

    added some query variables

--- Controller/updateCustomerTypeName.js
+++ Controller/updateCustomerTypeName.js
@@ -3,13 +3,13 @@ const asyncHandler = require("../utils/asynchandler");
 
 const updateCustomerType = asyncHandler(async (req, res) => {
 
-    const { customer_type_id, customer_type_name, customer_type_color, customer_type_text_color } = req.body
+    const { customer_type_id, customer_type_name, customer_type_color, customer_type_text_color, customer_type_priority } = req.body
 
     if (!customer_type_id || !customer_type_name || !customer_type_color || !customer_type_text_color) {
         return res.status(200).json({ ResponseCode: 0, message: "Both customer type id and name are required" })
     }
 
-    Connection.query("CALL SPupdatecustomertype(?, ?, ?, ?)", [customer_type_id, customer_type_name, customer_type_color, customer_type_text_color], (error, results) => {
+    Connection.query("CALL SPupdatecustomertype(?, ?, ?, ?, ?)", [customer_type_id, customer_type_name, customer_type_color, customer_type_text_color, customer_type_priority], (error, results) => {
         if (error) {
             console.log(error);
             return res.status(500).json({ ResponseCode: 0, message: "Error updating customer type, Try Again!!!" });
