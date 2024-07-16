a10133d644d11ab499ebf1645f5908525fdff5d6
softcron2024 <softcron2024@gmail.com>
2024-07-16 12:32:50 +0530

    added some query variables

--- Controller/addCustomerType.js
+++ Controller/addCustomerType.js
@@ -3,13 +3,13 @@ const asyncHandler = require("../utils/asynchandler");
 
 const addCustomerType = asyncHandler(async (req, res) => {
 
-    const { customer_type_name, customer_type_color, customer_type_text_color } = req.body
+    const { customer_type_name, customer_type_color, customer_type_text_color, customer_type_priority } = req.body
 
-    if (!customer_type_name || !customer_type_color || !customer_type_text_color) {
-        return res.status(200).json({ ResponseCode: 0, message: "Customer type name is required" })
+    if (!customer_type_name || !customer_type_color || !customer_type_text_color || !customer_type_priority) {
+        return res.status(200).json({ ResponseCode: 0, message: "All fields are required" })
     }
 
-    Connection.query("CALL SPinsertcustomertype(?, ?, ?)", [customer_type_name, customer_type_color, customer_type_text_color], (error, results) => {
+    Connection.query("CALL SPinsertcustomertype(?, ?, ?, ?)", [customer_type_name, customer_type_color, customer_type_text_color, customer_type_priority], (error, results) => {
         if (error) {
             console.log(error);
             return res.status(500).json({ ResponseCode: 0, message: "Error adding new customer type, Try Again!!!" });
