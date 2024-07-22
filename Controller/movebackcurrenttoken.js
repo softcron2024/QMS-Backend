c78a2f44e4693cdfe695e179ed813e6eaae1a83d
manishmanume <149936975+manishmanume@users.noreply.github.com>
2024-07-22 10:02:10 +0530

    change in move

--- Controller/movebackcurrenttoken.js
+++ Controller/movebackcurrenttoken.js
@@ -5,12 +5,14 @@ const moveBackCurrentToken = asyncHandler(async (req, res) => {
 
     const { token_no } = req.body
 
+    console.log(token_no);
+
     Connection.query("CALL SPmovebackinprocesstoken(?)", [token_no], (error, results) => {
         if (error) {
             console.log(error);
             return res.status(500).json({ ResponseCode: 0, message: "Error moving token back, Try Again!!!" });
         }
-        return res.status(200).json({ message: results })
+        return res.status(200).json({ message: results[0][0] })
     });
 })
 
