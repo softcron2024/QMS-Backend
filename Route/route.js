7ca895228985b5db0a6b16fde04749761248568c
softcron2024 <softcron2024@gmail.com>
2024-07-24 12:59:31 +0530

    format documents

--- Route/route.js
+++ Route/route.js
@@ -1,48 +1,49 @@
 const express = require("express");
 const router = express.Router();
-const { createToken } = require("../Controller/createToken.js");
-const { getUsers } = require("../Controller/allusers.js");
-const { getUser } = require("../Controller/getuser.js");
+
 const { settokenactivity } = require("../Controller/settokenactivity.js");
-const { enqueuetoken } = require("../Controller/enqueuetoken.js");
-const { createadmin } = require("../Controller/createadmin.js");
-const { handleLogin } = require("../Controller/adminlogin.js");
-const verify = require("../Middleware/verify.js");
-const { todayReport } = require("../Reporting/todayreport.js");
-const { weeklyReport } = require("../Reporting/weeklyReport.js");
-const { lastWeekReport } = require("../Reporting/lastWeekReport.js");
 const { thisMonthReport } = require("../Reporting/thiMonthReport.js");
+const { lastWeekReport } = require("../Reporting/lastWeekReport.js");
 const { thisYearReport } = require("../Reporting/thisYearReport.js");
+const { enqueuetoken } = require("../Controller/enqueuetoken.js");
+const { weeklyReport } = require("../Reporting/weeklyReport.js");
+const { createadmin } = require("../Controller/createadmin.js");
+const { createToken } = require("../Controller/createToken.js");
 const { cancelToken } = require("../Controller/cancelToken.js");
-const { callNextToken } = require("../Controller/callnexttoken.js");
-const { moveBackCurrentToken } = require("../Controller/movebackcurrenttoken.js");
-const { missedTokens } = require("../Controller/missedTokenList.js");
-const { recallMissedToken } = require("../Controller/recallMissedToken.js");
+const { todayReport } = require("../Reporting/todayreport.js");
 const { skipToken } = require("../Controller/skipToken.js");
-const { adjustTokenPosition } = require("../Controller/adjustTokenPosition.js");
+const { getUsers } = require("../Controller/allusers.js");
+const verify = require("../Middleware/verify.js");
+const { getUser } = require("../Controller/getuser.js");
+const { handleLogin } = require("../Controller/adminlogin.js");
+const { callNextToken } = require("../Controller/callnexttoken.js");
+const { missedTokens } = require("../Controller/missedTokenList.js");
 const { addCustomerType } = require("../Controller/addCustomerType.js");
-const { updateCustomerType } = require("../Controller/updateCustomerTypeName.js");
-const { deleteCustomerType } = require("../Controller/deleteCustomerType.js");
 const { getCustomerType } = require("../Controller/getCustomerType.js");
+const { recallMissedToken } = require("../Controller/recallMissedToken.js");
+const { deleteCustomerType } = require("../Controller/deleteCustomerType.js");
+const { adjustTokenPosition } = require("../Controller/adjustTokenPosition.js");
+const { moveBackCurrentToken } = require("../Controller/movebackcurrenttoken.js");
+const { updateCustomerType } = require("../Controller/updateCustomerTypeName.js");
 
 // Customer routes
+router.get("/getQueue",  enqueuetoken)
+router.get("/get-today-tokens", verify, getUsers)
+router.get("/get-customer-type", getCustomerType)
+router.get("/call-next-token", verify, callNextToken)
+router.get("/get-missed-tokens-list", verify, missedTokens)
+router.get("/move-back-current-token", verify, moveBackCurrentToken)
 
 router.post("/generate-token", verify, createToken);
-router.get("/get-today-tokens", verify, getUsers)
 router.post("/scan-token", verify, getUser)
 router.post("/setTokenActivity", verify, settokenactivity)
-router.get("/getQueue", verify, enqueuetoken)
 router.post("/cancel-token", verify, cancelToken)
-router.get("/call-next-token", verify, callNextToken)
-router.post("/move-back-current-token", verify, moveBackCurrentToken)
-router.get("/get-missed-tokens-list", verify, missedTokens)
 router.post("/recall-missed-token", verify, recallMissedToken)
 router.post("/skip-token", verify, skipToken)
 router.post("/adjust-token-position", verify, adjustTokenPosition)
 router.post("/add-customer-type", verify, addCustomerType)
 router.post("/delete-customer-type", verify, deleteCustomerType)
 router.post("/update-customer-type", verify, updateCustomerType)
-router.get("/get-customer-type", verify, getCustomerType)
 
 // Reporting routes
 
