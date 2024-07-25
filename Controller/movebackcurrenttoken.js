71f504c6a5e812759d4f7b3955ee3550c36c1bc8
softcron2024 <softcron2024@gmail.com>
2024-07-25 17:08:28 +0530

    format documents

--- Controller/movebackcurrenttoken.js
+++ Controller/movebackcurrenttoken.js
@@ -5,6 +5,10 @@ const moveBackCurrentToken = asyncHandler(async (req, res) => {
 
     const { token_no } = req.body
 
+    if (!token_no) {
+        return res.status(200).json({ ResponseCode: 0, message: "Token number is required" })
+    }
+
     Connection.query("CALL SPmovebackinprocesstoken(?)", [token_no], (error, results) => {
         if (error) {
             console.log(error);
