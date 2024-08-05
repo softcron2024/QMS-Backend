63d16d81df4d9b5d3f6740192227892033145340
manishmanume <149936975+manishmanume@users.noreply.github.com>
2024-08-05 10:00:07 +0530

    made Complete Token  API

--- Controller/completeToken.js
+++ Controller/completeToken.js
@@ -2,13 +2,16 @@ const Connection = require("../Connection");
 const asyncHandler = require("../utils/asynchandler");
 
 const CompleteToken = asyncHandler(async (req, res) => {
-    Connection.query("CALL SPcompletetoken()", (error, results) => {
+    const { token_no } = req.body;
+    console.log("Received token_no:", token_no);
+
+    Connection.query("CALL SPcompletetoken(?)", [token_no], (error, results) => {
         if (error) {
-            console.log(error);
+            console.error("Database Error:", error);
             return res.status(500).json({ ResponseCode: 0, message: "Error calling next token, Try Again!!!" });
         }
-        return res.status(200).json({ message: results[0][0] })
+        return res.status(200).json({ message: results[0][0] });
     });
-})
+});
 
-module.exports = { CompleteToken } 
+module.exports = { CompleteToken };
